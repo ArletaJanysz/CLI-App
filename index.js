9234aa706595098209ef95cfae5385ff32cf0d7a
ArletaJanysz <arleta.janysz@gmail.com>
2023-12-15 12:48:52 +0100

    functions' implementation

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const {
-	listContacts,
-	getContactById,
-	removeContact,
-	addContact,
-} = require("./contacts");
+const yargs = require("yargs");
+
+const argv = yargs.argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+	switch (action) {
+		case "list":
+			// implementacja
+			break;
+
+		case "get":
+			// implementacja
+			break;
+
+		case "add":
+			// implementacja
+			break;
+
+		case "remove":
+			// implementacja
+			break;
+
+		default:
+			console.warn("\x1B[31m Unknown action type!");
+	}
+}
+
+invokeAction(argv);
